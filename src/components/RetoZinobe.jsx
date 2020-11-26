5d209e6d551445b57a236e037a8434d0412cf9b0
Diana Rojas <diana.milenarb@gmail.com>
2020-11-26 18:20:01 -0500

    1. Inicialización proyecto, instalación babel y webpack

---
+++ src/components/RetoZinobe.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RetoZinobe = () => {
+    <h1>Reto Zinobe</h1>
+};
+
+export default RetoZinobe;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import RetoZinobe from './components/RetoZinobe';
+import HelloWord from './components/RetoZinobe';
+
+ReactDOM.render(<RetoZinobe />, document.getElementById(app));
+
