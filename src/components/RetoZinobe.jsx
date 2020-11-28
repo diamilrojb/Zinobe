ce215a6af54b8b473a5354876e950922d5052072
Diana Rojas <diana.milenarb@gmail.com>
2020-11-28 02:17:34 -0500

    3. Creación archivos .eslintrc y .gitinore

--- src/components/RetoZinobe.jsx
+++ src/components/RetoZinobe.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/styles/App.scss';
 
 const RetoZinobe = () => (
     <h1>Reto Zinobe</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
@@ -25,6 +27,16 @@ module.exports = {
                         loader: 'html-loader'
                 },
             },
+                {
+                    test: /\.(s*)css$/,
+                    use:[ 
+                        {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader',
+                     ],
+                },
         ],
     },
     plugins:[
@@ -32,5 +44,8 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html',
         }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
     ],
 };
