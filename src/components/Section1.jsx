f6fefb21cbe04be51af2d08d5b8a4ada4cf575a5
Miguel porras <porraangel@gmail.com>
2020-12-13 23:55:47 -0500

    agregando la api

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import '../assets/styles/components/Section1.scss';
 import foto from '../assets/static/Recurso9.png';
 
-const Section1 = () => (
+const Section1 = ( { _id, Nombre, Apellido, Telefono, Cedula, Cargo, Estado, Id_estado,Puntos_disponibles, Puntos_redimidos } ) => (
+
+
+
+  console.log( _id, Nombre, Apellido, Telefono, Cedula, Cargo, Estado, Id_estado, Puntos_disponibles, Puntos_redimidos),
   <section className='main-section1'>
     <div>
       <img className='main-section1__user--img' src={foto} alt='Foto' />
     </div>
     <div className='main-section1__user'>
       <div className='main-section1__user--data'>
-        <p>Nombre: </p>
-        <p>Rol: </p>
+        <p>Nombre: {Nombre+ " " + Apellido} </p>
+        <p>Rol:  { Cargo }  </p>
         <p>Equipo: </p>
       </div>
       <div className='main-section1__user--gamification'>
+      
         <p>PUNTOS DISPONIBLES </p>
+        <p>{ Puntos_disponibles}</p>
       </div>
       <div className='main-section1__user--gamification'>
-        <p>PUNTOS GASTADOS </p>
+    
+        <p>PUNTOS REDIMIDOS </p>
+        <p>{ Puntos_redimidos}</p>
       </div>
     </div>
   </section>
